54a1ae432b4921cb10ff5e8c754ff0ee0e8816c6
sahilmor <mor.sahil05.28@gmail.com>
2024-03-24 15:13:25 +0530

    begining

---
+++ script.js
@@ -0,0 +1,11 @@
+var calorieCounter = document.getElementById("calorie-counter");
+var budgetNumberInput = document.getElementById( "budget" );
+var entryDropdown = document.getElementById("entry-dropdown");
+var addEntryButton = document.getElementById( "add-entry" ) ;
+var clearButton = document.getElementById( "clear" ) ;
+var output = document.getElementById( "output" ) ;
+let isError = false;
+
+function cleanInputString(str){
+    const regex = /\+-/;
+}
